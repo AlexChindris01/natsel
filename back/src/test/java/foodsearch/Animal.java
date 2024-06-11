62e7a1bff15e5984b2a4364eb6cba8c2570880a6
AlexChindris01 <chindrisalex01@gmail.com>
2024-06-11 20:18:15 +0300

    chase previously detected food (without checking again if any food is detected)

--- back/src/test/java/foodsearch/Animal.java
+++ back/src/test/java/foodsearch/Animal.java
@@ -10,43 +10,63 @@ public class Animal {
     double direction;
     double sight;
     double speed;
-    double stepSize;
+    int foodEaten;
+    Point chasedFood;
     public Animal(double x, double y) {
         location = new Point(x, y);
         direction = 0;
         sight = 5;
         speed = 8;
+        chasedFood = null;
+        foodEaten = 0;
     }
     public Animal() {
         location = new Point(0, 0);
         direction = 0;
         sight = 5;
         speed = 8;
+        chasedFood = null;
+        foodEaten = 0;
     }
     public void searchStep() {
-        Random rand = new Random();
-        direction += -PI / 2 + PI * rand.nextFloat();
-        Point step = new Point(location.x + Math.cos(direction) * speed, location.y + Math.sin(direction) * speed);
-        Point testFood = new Point(3, 3);
-        Point foodDetectionPoint = CustomMath.line_circle_intersection(location, step, testFood, sight);
-        if (foodDetectionPoint == null) {
-            location = step;
-        }
-        else {
-            double stepRemainder = CustomMath.dist(foodDetectionPoint, step);
-            double ratio = stepRemainder / CustomMath.dist(foodDetectionPoint, testFood);
+        if (chasedFood != null) {
+            double ratio = speed / CustomMath.dist(location, chasedFood);
             if (ratio >= 1) {
-                location = testFood;
-
+                location = chasedFood;
+                foodEaten++;
+                chasedFood = null;
                 // to implement: update amount of food eaten, make the food item disappear etc
-
             }
             else {
-                location = foodDetectionPoint;
-                location.x += ratio * (testFood.x - foodDetectionPoint.x);
-                location.y += ratio * (testFood.y - foodDetectionPoint.y);
-                // to implement: head towards food immediately at the next step
+                location.x += ratio * (chasedFood.x - location.x);
+                location.y += ratio * (chasedFood.y - location.y);
             }
         }
+        else {
+            Random rand = new Random();
+            direction += -PI / 2 + PI * rand.nextFloat();
+            Point step = new Point(location.x + Math.cos(direction) * speed, location.y + Math.sin(direction) * speed);
+            Point testFood = new Point(3, 3);
+            Point foodDetectionPoint = CustomMath.line_circle_intersection(location, step, testFood, sight);
+            if (foodDetectionPoint == null) {
+                location = step;
+            }
+            else {
+                double stepRemainder = CustomMath.dist(foodDetectionPoint, step);
+                double ratio = stepRemainder / CustomMath.dist(foodDetectionPoint, testFood);
+                if (ratio >= 1) {
+                    location = testFood;
+                    foodEaten++;
+                    // to implement: update amount of food eaten, make the food item disappear etc
+                }
+                else {
+                    chasedFood = testFood;
+                    location = foodDetectionPoint;
+                    location.x += ratio * (testFood.x - foodDetectionPoint.x);
+                    location.y += ratio * (testFood.y - foodDetectionPoint.y);
+                }
+            }
+        }
+
     }
 }

189c7c92a4c032c3f39d165f5b30ddca68bb5f17
AlexChindris01 <chindrisalex01@gmail.com>
2024-06-11 15:30:29 +0300

    some more food detection work

--- back/src/test/java/foodsearch/Animal.java
+++ back/src/test/java/foodsearch/Animal.java
@@ -6,23 +6,47 @@ import java.lang.Math.*;
 import static java.lang.Math.PI;
 
 public class Animal {
-    double x, y;
+    Point location;
     double direction;
+    double sight;
+    double speed;
     double stepSize;
     public Animal(double x, double y) {
-        this.x = x;
-        this.y = y;
+        location = new Point(x, y);
         direction = 0;
+        sight = 5;
+        speed = 8;
     }
     public Animal() {
-        x = 0;
-        y = 0;
+        location = new Point(0, 0);
         direction = 0;
+        sight = 5;
+        speed = 8;
     }
     public void searchStep() {
         Random rand = new Random();
         direction += -PI / 2 + PI * rand.nextFloat();
-        x += Math.cos(direction);
-        y += Math.sin(direction);
+        Point step = new Point(location.x + Math.cos(direction) * speed, location.y + Math.sin(direction) * speed);
+        Point testFood = new Point(3, 3);
+        Point foodDetectionPoint = CustomMath.line_circle_intersection(location, step, testFood, sight);
+        if (foodDetectionPoint == null) {
+            location = step;
+        }
+        else {
+            double stepRemainder = CustomMath.dist(foodDetectionPoint, step);
+            double ratio = stepRemainder / CustomMath.dist(foodDetectionPoint, testFood);
+            if (ratio >= 1) {
+                location = testFood;
+
+                // to implement: update amount of food eaten, make the food item disappear etc
+
+            }
+            else {
+                location = foodDetectionPoint;
+                location.x += ratio * (testFood.x - foodDetectionPoint.x);
+                location.y += ratio * (testFood.y - foodDetectionPoint.y);
+                // to implement: head towards food immediately at the next step
+            }
+        }
     }
 }
